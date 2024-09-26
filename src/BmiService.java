ee03e4b20120eca5b6841bcb648f3e9e7ac6ba0f
Taty ts <tatytsareva@mail.ru>
2024-09-26 10:20:18 +0300

    initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,31 @@
+
+public class BmiService {
+    public int calculate(double weight, double height) {
+
+        double result = weight / (height * height);
+        if (result<18){
+            System.out.println("Недостаточная масса тела");
+        }
+        if (result>=20 && result<=24){
+            System.out.println("Нормальный вес, не ешьте больше");
+        }
+        if (result>=30){
+            System.out.println("Остановитесь, не надо больше есть");
+        }
+
+        return (int)result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        double weight = 150;
+        double height = 1.69;
+        double index = service.calculate(weight,height);
+
+        System.out.println ("Индекс массы тела"+ " " + (int)index);
+
+
+    }
+}
